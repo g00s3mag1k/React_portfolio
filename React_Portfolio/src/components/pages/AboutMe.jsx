ad2f182989eef479ee6b7cc20b6a4e5b97e7fb76
Brandon <beegee54321@gmail.com>
2024-04-11 23:15:20 -0700

    adding code to aboutme/contact/error saving progres

---
+++ React_Portfolio/src/components/pages/AboutMe.jsx
@@ -0,0 +1,15 @@
+export default function About() {
+    return (
+      <div>
+        <h1>About Me</h1>
+        <p>
+        Hello!! Welcome to my Portfolio Page! My name is Brandon Gonzalez, I have recently enrolled into a bootcamp program through the University of San Diego for web development. 
+        I am excited to showcase my skills and expertise on this personal web page as I continue through the course in the next six months. 
+        I will be learning new web development techniques throughout my educational journey and implement them accordingly, I'm going to be updating my website as I grasp onto the role of a full-stack web developer. 
+        I am only three weeks into my cyber voyage and look forward to all the challenges that lie ahead of me so that I can make a successful career change into the tech industry. 
+        I am working my way through the fundamentals and aquiring the necessary steps to understand the basics and advanced techniques. I have always been a quick learner and wish to extend that skill into the web devlopment world by applying my efforts and understanding code.
+        </p>
+      </div>
+    );
+  }
+  
---
+++ React_Portfolio/src/components/pages/Contact.jsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <p>
+          Please feel free to contact me about any job opportunities!
+        </p>
+      </div>
+    );
+  }
+  
