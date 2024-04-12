b9fad2a8907caad2b8054490556accd686b1e553
Brandon <beegee54321@gmail.com>
2024-04-12 09:42:18 -0700

    trying to deploy to netlify but keep gettin error

--- vite.config.js
+++ vite.config.js
@@ -8,20 +8,4 @@ export default defineConfig({
     port: 3000,
     open: true
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-        // Add the following line to set the correct MIME type
-        scriptType: 'module',
-      },
-    },
-  },
 });
